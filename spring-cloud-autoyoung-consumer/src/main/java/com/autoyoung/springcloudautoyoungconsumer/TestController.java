7ce14bf9584ae213bec1a92f16007e2e4125e16f
liuyang <me@liuyangjob.cn>
2020-04-13 18:02:35 +0800

    liuyang
    lll

--- spring-cloud-autoyoung-consumer/src/main/java/com/autoyoung/springcloudautoyoungconsumer/TestController.java
+++ spring-cloud-autoyoung-consumer/src/main/java/com/autoyoung/springcloudautoyoungconsumer/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @GetMapping("hello")
     public String hello(@RequestParam String name) {
-        return "hello" + name;
+        return "hello " + name;
     }
 }
