838e431e21106962cd118c7d8c7e27d47a4b83ec
liuyang <me@liuyangjob.cn>
2020-04-13 17:31:34 +0800

    liuyang
    add depenceis

---
+++ spring-cloud-autoyoung-consumer/src/main/java/com/autoyoung/springcloudautoyoungconsumer/SpringCloudAutoyoungConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.autoyoung.springcloudautoyoungconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAutoyoungConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAutoyoungConsumerApplication.class, args);
+    }
+
+}
